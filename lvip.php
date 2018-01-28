79c0e93eab867d71ded668e699b58caf92100dae
dzintars <dzintars@bergs.id.lv>
2018-01-28 22:08:07 +0200

    init commit

---
+++ lvip.php
@@ -0,0 +1,28 @@
+<?php
+
+header('Content-Type: text/plain');
+$source = 'https://www.nic.lv/local.net';
+$list = 'lv';
+
+$ips = [];
+if ($file = fopen($source, 'rb')) {
+    while (!feof($file)) {
+        $line = trim(fgets($file));
+        if (empty($line) || 0 === strpos($line, '#')) {
+            continue;
+        }
+        $ips[] = trim($line);
+    }
+    fclose($file);
+}
+$ips = array_unique($ips);
+
+if (empty($ips)){
+    die("# failed to get ip list\n");
+}
+
+echo "/ip firewall address-list\n";
+echo "remove [/ip firewall address-list find list=$list]\n";
+foreach ($ips as $ip) {
+    echo "add list=$list address=$ip\n";
+}
